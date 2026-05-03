620bfbbfb05a42d54e97f0b82350003134af6aa0
wb.fengqingqing <wb.fengqingqing@mesg.corp.netease.com>
2026-05-03 19:27:34 +0800

    新增 Lugwit_PackageRegistry 初始项目结构与包注册表配置
    添加 .gitignore 忽略编译产物与虚拟环境，创建 999.0 版本的 package.py 定义包元数据，并引入 package_registry.yaml 作为 wuwo auto_fetch 的单一数据源，集中管理 GitHub、pip 和 NuGet 包的注册信息。

---
+++ 999.0/package.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+name = "Lugwit_PackageRegistry"
+version = "999.0"
+description = "Central Rez package registry (github / pip / nuget) for wuwo auto_fetch"
+authors = ["Lugwit Team"]
+
+requires = []
+
+build_command = False
+cachable = True
+relocatable = True
+
+
+def commands():
+    pass
